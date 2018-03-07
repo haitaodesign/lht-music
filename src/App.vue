f8238c86b16a87d14312208b2e0bd1b0aede4fa9
lihaitao <1775814590@qq.com>
2018-03-07 23:23:34 +0800

    新增 init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="stylus" scoped>
+  @import 'common/stylus/variable';
+
+  #app {
+    color: $color-theme;
+  }
+</style>
+
