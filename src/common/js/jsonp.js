91c89d1db8b8a0e947d60402072aa10133615b51
lihaitao <1775814590@qq.com>
2018-03-11 20:17:25 +0800

    新增 jsonp封装

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,22 @@
+import originJsonp from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+export function param(data) {
+  let url = ''
+  for (const key in object) {
+    let value = data[key] !== undefined ? data[k] : ''
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
+}
