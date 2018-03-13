36a77f9766873839dc02685158af1b47a93c4cd1
lihaitao <1775814590@qq.com>
2018-03-13 23:39:27 +0800

    修复 eslint报错

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,8 +1,7 @@
 export function hasClass(el, className) {
-  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)');
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
 }
-
 export function addClass(el, className) {
   if (hasClass(el, className)) {
     return
