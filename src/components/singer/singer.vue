126d2e9833d8e0c3bc64328b0399440bee0b3e31
lihaitao <1775814590@qq.com>
2018-03-18 16:09:30 +0800

    修改 页面获取歌手数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,7 +5,23 @@
 </template>
 
 <script>
-    export default {}
+    import {getSingerList} from 'api/singer.js'
+    export default {
+        data(){
+            return{
+                singers:[]
+            }
+        },
+        mounted(){
+            this._getSingerList()
+        },
+        methods:{
+            async _getSingerList(){
+                const res = await getSingerList()
+                this.singers=res.data.list
+            }
+        }
+    }
 </script>
 
 <style scoped>
