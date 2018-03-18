c7e01a7e94de81cb55e17d438e5bb61de18468f8
lihaitao <1775814590@qq.com>
2018-03-18 21:27:02 +0800

    修改 修复scroll组件方法bug

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -49,7 +49,7 @@
                 this.scroll && this.scroll.scrollTo.apply(this.scroll,arguments)
             },
             scrollToElement(){
-                this.scroll && this.scroll.scrollTo.apply(this.scroll,arguments)
+                this.scroll && this.scroll.scrollToElement.apply(this.scroll,arguments)
             }
         },
         watch:{
