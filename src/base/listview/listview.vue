44cceef253cae0683ac6e5e651af0c4ea7b38d66
lihaitao <1775814590@qq.com>
2018-03-19 22:26:11 +0800

    完成 listview组件功能完成

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -23,14 +23,22 @@
                 :class="{'current':currentIndex===index}">{{item}}</li>
             </ul>
         </div>
+        <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+            <h1 class="fixed-title">{{fixedTitle}}</h1>
+        </div>
+        <div v-show="!data.length" class="loading-container">
+            <loading></loading>
+        </div>
     </scroll>
 </template>
 
 <script>
     import Scroll from '@/base/scroll/scroll'
+    import Loading from '@/base/loading/loading'
     import {getData} from 'common/js/dom.js'
 
     const ANCHOR_HEIGHT = 18
+    const TITLE_HEIGHT = 30
     export default {
         props:{
             data:{
@@ -45,12 +53,19 @@
                 return this.data.map((group)=>{
                     return group.title.substr(0,1)
                 })
+            },
+            fixedTitle(){
+                if(this.scrollY>0){
+                    return ''
+                }
+                return this.data[this.currentIndex] ? this.data[this.currentIndex].title:''
             }
         },
         data(){
             return{
                 scrollY:-1,
-                currentIndex:0
+                currentIndex:0,
+                diff:-1
             }
         },
         created(){
@@ -80,6 +95,16 @@
                 this.scrollY=pos.y
             },
             _scorllTo(index){
+                if(!index && index!==0){
+                    return
+                }
+                if(index<0){
+                    index=0
+                }else if(index>this.listHeight.length-2){
+                    index = this.listHeight-2
+                }
+
+                this.scrollY = -this.listHeight[index]
                 this.$refs.listview.scrollToElement(this.$refs.listGroup[index],0)
             },
             _calculateHeight(){
@@ -102,19 +127,33 @@
             },
             scrollY(newY){
                 const listHeight = this.listHeight
-                for (let i = 0; i < listHeight.length; i++) {
+                if(newY>0){
+                    this.currentIndex=0
+                    return
+                }
+                for (let i = 0; i < listHeight.length-1; i++) {
                     let height1 = listHeight[i]
                     let height2 = listHeight[i+1]
-                    if(!height2 || (-newY>height1 && -newY <height2)){
+                    if(-newY>=height1 && -newY<height2){
                         this.currentIndex = i
+                        this.diff = height2 + newY
                         return
                     }
                 }
-                return i
+                this.currentIndex = listHeight.length - 2
+            },
+            diff(newVal) {
+                let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+                if (this.fixedTop === fixedTop) {
+                return
+                }
+                this.fixedTop = fixedTop
+                this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
             }
         },
         components:{
-            Scroll
+            Scroll,
+            Loading
         }
     }
 </script>
