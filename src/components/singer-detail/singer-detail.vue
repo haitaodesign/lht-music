f8a204e8603cc9421ffdf5a63c7542f2dad62eaf
lihaitao <1775814590@qq.com>
2018-03-23 08:56:08 +0800

    新增 歌手详情数据爬取以及处理

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -7,6 +7,8 @@
 <script>
     import MusicList from '../music-list/music-list'
     import {getSingerDetail} from '@/api/singer'
+    import {createSong} from 'common/js/song'
+    import {ERR_OK} from "@/api/config"
     import {mapGetters} from 'vuex'
     export default {
         components:{
@@ -16,6 +18,11 @@
              ...mapGetters([
                  'singer'
              ])
+        },
+        data(){
+            return{
+                songs:[]
+            }
         },
         mounted(){
             this._getSingerDetail()
@@ -28,7 +35,19 @@
                     })
                 }
                 const data = await getSingerDetail(this.singer.id)
-                console.log(data)
+                if(data.code==ERR_OK){
+                    this.songs = this._normalizeSong(data.data.list)
+                }
+            },
+            _normalizeSong(list){
+                let ret = []
+                list.forEach((item) => {
+                    let {musicData} = item
+                    if (musicData.songid && musicData.albummid) {
+                    ret.push(createSong(musicData))
+                }
+            })
+            return ret
             }
         }
     }
